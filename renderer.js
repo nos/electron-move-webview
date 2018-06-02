a9091bc9ae48ed37c9ae81ae9edcc3a5930668f3
Matt Huggins <matt.huggins@gmail.com>
2018-06-02 18:21:14 -0500

    Added sample code for moving guestinstance not working

--- renderer.js
+++ renderer.js
@@ -1,3 +1,30 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+function moveWebview(existingWebview, container) {
+  const webview = document.createElement('webview');
+  const { attributes } = existingWebview;
+  const originalGuestInstance = existingWebview.guestinstance;
+
+  console.log('original guestinstance', existingWebview.guestinstance);
+
+  for (let i = 0; i < attributes.length; i += 1) {
+    webview.setAttribute(attributes[i].nodeName, attributes[i].nodeValue);
+  }
+
+  console.log('copied guestinstance', webview.guestinstance);
+
+  container.appendChild(webview);
+  existingWebview.parentNode.removeChild(existingWebview);
+
+  return webview;
+}
+
+window.addEventListener("load", () => {
+  const button = document.getElementById("button");
+  const webviews = document.getElementById("webviews");
+
+  let index = 0;
+
+  button.addEventListener("click", () => {
+    index = (index + 1) % 2;
+    moveWebview(document.getElementById("webview"), webviews.children[index]);
+  });
+});
